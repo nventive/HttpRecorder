12e1d03e27e2e494c9cbd1d9c804e7c0b4cfd4fd
Julien Blin <julien.blin@gmail.com>
2020-04-06 17:31:22 -0400

    Added HttpRecorderContext.

---
+++ HttpRecorder.Tests/ContextTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HttpRecorder.Context;
+using HttpRecorder.Tests.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HttpRecorder.Tests
+{
+    [Collection(ServerCollection.Name)]
+    public class ContextTests
+    {
+        private readonly ServerFixture _fixture;
+
+        public ContextTests(ServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task ItShouldWorkWithHttpRecorderContext()
+        {
+            var services = new ServiceCollection();
+            services
+                .AddHttpRecorderContextSupport()
+                .AddHttpClient(
+                    "TheClient",
+                    options =>
+                    {
+                        options.BaseAddress = _fixture.ServerUri;
+                    });
+
+            HttpResponseMessage passthroughResponse = null;
+            using (var context = new HttpRecorderContext((sp, builder) => new HttpRecorderConfiguration
+            {
+                Mode = HttpRecorderMode.Record,
+                InteractionName = nameof(ItShouldWorkWithHttpRecorderContext),
+            }))
+            {
+                var client = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient("TheClient");
+                passthroughResponse = await client.GetAsync(ApiController.JsonUri);
+                passthroughResponse.EnsureSuccessStatusCode();
+            }
+
+            using (var context = new HttpRecorderContext((sp, builder) => new HttpRecorderConfiguration
+            {
+                Mode = HttpRecorderMode.Replay,
+                InteractionName = nameof(ItShouldWorkWithHttpRecorderContext),
+            }))
+            {
+                var client = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient("TheClient");
+                var response = await client.GetAsync(ApiController.JsonUri);
+                response.EnsureSuccessStatusCode();
+                response.Should().BeEquivalentTo(passthroughResponse);
+            }
+        }
+
+        [Fact]
+        public async Task ItShouldWorkWithHttpRecorderContextWhenNotRecording()
+        {
+            var services = new ServiceCollection();
+            services
+                .AddHttpRecorderContextSupport()
+                .AddHttpClient(
+                    "TheClient",
+                    options =>
+                    {
+                        options.BaseAddress = _fixture.ServerUri;
+                    });
+
+            HttpResponseMessage passthroughResponse = null;
+            using (var context = new HttpRecorderContext((sp, builder) => new HttpRecorderConfiguration
+            {
+                Enabled = false,
+                Mode = HttpRecorderMode.Record,
+                InteractionName = nameof(ItShouldWorkWithHttpRecorderContextWhenNotRecording),
+            }))
+            {
+                var client = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient("TheClient");
+                passthroughResponse = await client.GetAsync(ApiController.JsonUri);
+                passthroughResponse.EnsureSuccessStatusCode();
+            }
+
+            using (var context = new HttpRecorderContext((sp, builder) => new HttpRecorderConfiguration
+            {
+                Mode = HttpRecorderMode.Replay,
+                InteractionName = nameof(ItShouldWorkWithHttpRecorderContextWhenNotRecording),
+            }))
+            {
+                var client = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient("TheClient");
+                Func<Task> act = async () => await client.GetAsync(ApiController.JsonUri);
+                act.Should().Throw<HttpRecorderException>();
+            }
+        }
+
+        [Fact]
+        public void ItShouldNotAllowMultipleContexts()
+        {
+            using var context = new HttpRecorderContext();
+            Action act = () => { var ctx2 = new HttpRecorderContext(); };
+            act.Should().Throw<HttpRecorderException>().WithMessage("*multiple*");
+        }
+    }
+}
---
+++ HttpRecorder/Context/RecorderHttpMessageHandlerBuilderFilter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Http;
+
+namespace HttpRecorder.Context
+{
+    /// <summary>
+    /// <see cref="IHttpMessageHandlerBuilderFilter"/> that adds <see cref="HttpRecorderDelegatingHandler"/>
+    /// based on the value of <see cref="HttpRecorderContext.Current"/>.
+    /// </summary>
+    public class RecorderHttpMessageHandlerBuilderFilter : IHttpMessageHandlerBuilderFilter
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecorderHttpMessageHandlerBuilderFilter"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The <see cref="IServiceProvider"/>.</param>
+        public RecorderHttpMessageHandlerBuilderFilter(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        /// <inheritdoc />
+        public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next)
+        {
+            return (builder) =>
+            {
+                // Run other configuration first, we want to decorate.
+                next(builder);
+
+                var context = HttpRecorderContext.Current;
+                if (context is null)
+                {
+                    return;
+                }
+
+                var config = context.ConfigurationFactory?.Invoke(_serviceProvider, builder) ?? new HttpRecorderConfiguration();
+
+                if (config.Enabled)
+                {
+                    var interactionName = config.InteractionName;
+                    if (string.IsNullOrEmpty(interactionName))
+                    {
+                        interactionName = Path.Combine(
+                            Path.GetDirectoryName(context.FilePath),
+                            $"{Path.GetFileNameWithoutExtension(context.FilePath)}Fixtures",
+                            context.TestName,
+                            builder.Name);
+                    }
+
+                    builder.AdditionalHandlers.Add(new HttpRecorderDelegatingHandler(
+                        interactionName,
+                        mode: config.Mode,
+                        matcher: config.Matcher,
+                        repository: config.Repository,
+                        anonymizer: config.Anonymizer));
+                }
+            };
+        }
+    }
+}
